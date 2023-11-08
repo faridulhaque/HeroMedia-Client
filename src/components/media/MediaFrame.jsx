bc3b63d0844b3985e5b50366a61b100c584a0c1f
faridulhaque <faridulhaquemurshed@gmail.com>
2023-11-09 02:17:37 +0600

    blog page started

--- src/components/media/MediaFrame.jsx
+++ src/components/media/MediaFrame.jsx
@@ -8,25 +8,24 @@ const MediaFrame = ({ blog }) => {
     blog?.userEmail
   );
 
-  
-
   return (
     <div className="card lg:card-side bg-base-200 shadow-xl lg:w-2/4 sm:11/12 mx-auto my-5">
-      <figure className="w-2/4">
-        <img
-          src={blog?.imagePath}
-          alt="Album"
-          className="h-auto"
-        />
+      <figure className="lg:w-2/4 sm:4/5">
+        <img src={blog?.imagePath} alt="Album" className="w-full h-full" />
       </figure>
-      <div className="card-body w-2/4">
+      <div className="card-body lg:w-2/4 sm:4/5">
         <h2 className="card-title">{blog?.title}</h2>
-        <p className="text-lg">{profileData?.displayName}</p>
-        <p className="text-lg">{moment(profileData?.date).format('MMMM Do YYYY, h:mm:ss a')}</p>
-        
-        <p>{
-          blog?.blog?.slice(0, 150) + "... "}<Link className="link text-blue-300" to={`/blog/${blog?._id}`}>See More</Link></p>
+        <p className="text-sm">
+          {moment(profileData?.date).format("MMMM Do YYYY, h:mm:ss a")}
+        </p>
+        <p className="text-lg">By {profileData?.displayName}</p>
 
+        <p>
+          {blog?.blog?.slice(0, 150) + "... "}
+          <Link className="link text-blue-300" to={`/blog/${blog?._id}`}>
+            See More
+          </Link>
+        </p>
       </div>
     </div>
   );
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase.init";
+import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
 
 const BlogPage = () => {
-    return (
-        <div>
-            <h2>this is for blog</h2>
-        </div>
-    );
+  const [user] = useAuthState(auth);
+
+  return (
+    <>
+      <Navbar userEmail={user?.email}></Navbar>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default BlogPage;
