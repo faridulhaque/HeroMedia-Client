a4c175a05bd964aed7745ee0201b917f54b1d85a
faridulhaque <faridulhaquemurshed@gmail.com>
2023-11-11 21:27:14 +0600

    done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyD7DjansFiS1pyY70sZ_iS0IjfxKlEDD7k",
+  authDomain: "heromedia-59b2a.firebaseapp.com",
+  projectId: "heromedia-59b2a",
+  storageBucket: "heromedia-59b2a.appspot.com",
+  messagingSenderId: "870571687208",
+  appId: "1:870571687208:web:4329e1685efc39588b641d",
 };
 
 const app = initializeApp(firebaseConfig);
